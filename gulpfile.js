e7c44848370f549f1130d691e18e28fe57a4a003
Maxim <79407225+MaximLeff@users.noreply.github.com>
2021-10-29 15:30:12 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,45 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+var sass = require ('gulp-sass') (require ('sass'));
+var autoprefixer = require('gulp-autoprefixer');
+// var group_media = require('gulp-group-css-media-queries');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+let clean_css = require('gulp-clean-css');
+
+// название темы
+var themeName = 'fitness-hub';
+
+// название темы
+var domName = '11test.loc';
+
+
 
 gulp.task('sass', function(done) {
-    gulp.src("scss/*.scss") // путь к scss файлам
+    gulp.src("wp-content/themes/" + themeName + "/scss/*.scss") // путь к scss файлам
+        .pipe(sourcemaps.init())
+        
         .pipe(sass())
-        .pipe(gulp.dest("css/")) //папка для выгрузки css файлов
+        // .pipe(group_media())
+        .pipe(
+            autoprefixer({
+                overrideBrowserslist: ['last 5 versions'],
+                cascade: true
+            })
+            )
+            .pipe(clean_css({ 
+                level: { 2: 
+                    { restructureRules: true } 
+                } 
+            }))
+            .pipe(
+                rename({
+                    extname: ".min.css"
+                })
+                )
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest("wp-content/themes/" + themeName + "/css")) //папка для выгрузки css файлов
+            
         .pipe(browserSync.stream());
     done();
 });
@@ -13,16 +47,17 @@ gulp.task('sass', function(done) {
 gulp.task('serve', function(done) {
 
     browserSync.init({
-        proxy: 'http://ukrArtist3.loc/',
-        host: 'ukrArtist3.loc',
+        proxy: 'http://' + domName + ':8080/',
+        host: domName,
         open: 'external'
     });
 
-    gulp.watch("scss/*.scss", gulp.series('sass')).on('change', () => {
+    gulp.watch("wp-content/themes/" + themeName + "/scss/*.scss", gulp.series('sass')).on('change', () => {
         browserSync.reload();
         done();
-        });
-    gulp.watch("*.php").on('change', () => {
+    });
+
+    gulp.watch("**/*.php").on('change', () => {
         browserSync.reload();
         done();
     });
