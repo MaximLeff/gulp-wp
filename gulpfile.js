b4ee0ef1785b5439e7276d0124a81f97282a82e1
Maxim <79407225+MaximLeff@users.noreply.github.com>
2021-10-29 21:17:34 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -2,52 +2,24 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require ('gulp-sass') (require ('sass'));
 var autoprefixer = require('gulp-autoprefixer');
-// var group_media = require('gulp-group-css-media-queries');
+var group_media = require('gulp-group-css-media-queries');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 let clean_css = require('gulp-clean-css');
+let ttf2woff = require('gulp-ttf2woff');
+let ttf2woff2 = require('gulp-ttf2woff2');
 
 // название темы
-var themeName = 'fitness-hub';
+var themeName = 'UkrArtists';
 
 // название темы
-var domName = '11test.loc';
-
-
-
-gulp.task('sass', function(done) {
-    gulp.src("wp-content/themes/" + themeName + "/scss/*.scss") // путь к scss файлам
-        .pipe(sourcemaps.init())
-        
-        .pipe(sass())
-        // .pipe(group_media())
-        .pipe(
-            autoprefixer({
-                overrideBrowserslist: ['last 5 versions'],
-                cascade: true
-            })
-            )
-            .pipe(clean_css({ 
-                level: { 2: 
-                    { restructureRules: true } 
-                } 
-            }))
-            .pipe(
-                rename({
-                    extname: ".min.css"
-                })
-                )
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest("wp-content/themes/" + themeName + "/css")) //папка для выгрузки css файлов
-            
-        .pipe(browserSync.stream());
-    done();
-});
+var domName = 'ukrArtist2.loc';
 
 gulp.task('serve', function(done) {
 
     browserSync.init({
-        proxy: 'http://' + domName + ':8080/',
+        // proxy: 'http://' + domName + ':8080/',
+        proxy: 'http://' + domName,
         host: domName,
         open: 'external'
     });
@@ -65,4 +37,65 @@ gulp.task('serve', function(done) {
     done();
 });
 
-gulp.task('default', gulp.series('sass', 'serve'));
+// scss -> min.css
+gulp.task('sass', function(done) {
+    gulp.src("wp-content/themes/" + themeName + "/scss/main.scss") // путь к scss файлам
+        .pipe(sourcemaps.init())
+        
+        .pipe(sass())
+        // .pipe(group_media())
+        .pipe(
+            autoprefixer({
+                overrideBrowserslist: ['last 5 versions'],
+                cascade: true
+            })
+            )
+        .pipe(clean_css({ 
+            level: { 2: 
+                { restructureRules: true } 
+            } 
+        }))
+        .pipe(
+            rename({
+                extname: ".min.css"
+            })
+            )
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest("wp-content/themes/" + themeName + "/css")) //папка для выгрузки css файлов
+            
+        .pipe(browserSync.stream());
+    done();
+});
+
+// scss -> css
+gulp.task('sass-css', function(done) {
+    gulp.src("wp-content/themes/" + themeName + "/scss/main.scss") // путь к scss файлам
+        .pipe(sourcemaps.init())
+        
+        .pipe(sass())
+        .pipe(group_media())
+        .pipe(
+            autoprefixer({
+                overrideBrowserslist: ['last 5 versions'],
+                cascade: true
+            })
+            )
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest("wp-content/themes/" + themeName + "/css")) //папка для выгрузки css файлов
+            
+        .pipe(browserSync.stream());
+    done();
+});
+
+gulp.task('fonts', function(done) {
+	gulp.src("wp-content/themes/" + themeName + "/fonts/ttf/**/*.ttf")
+		.pipe(ttf2woff())
+		.pipe(gulp.dest("wp-content/themes/" + themeName + "/fonts/woff"));
+	gulp.src("wp-content/themes/" + themeName + "/fonts/ttf/**/*.ttf")
+		.pipe(ttf2woff2())
+		.pipe(gulp.dest("wp-content/themes/" + themeName + "/fonts/woff2"));
+        done();
+});
+
+
+gulp.task('default', gulp.series('sass', 'sass-css', 'serve'), gulp.parallel('fonts'));
